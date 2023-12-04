195133f371964570b359d8e5842c18348d5a34ea
Anthony Cabacungan <antcabacungan@csu.fullerton.edu>
2023-12-03 21:50:26 -0800

    edited image styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -47,7 +47,7 @@ export default function Profile() {
             fit={'cover'}
             align={'center'}
             w={'100%'}
-            h={{ base: '100%', sm: '400px', lg: '500px' }}
+            h={{ base: '100%', sm: '350px', lg: '450px' }}
           />
         </Flex>
         <Stack spacing={{ base: 6, md: 10 }}>
